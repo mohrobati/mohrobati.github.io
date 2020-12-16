4007cd3732047fdfa1bac02af9d41f3fbe4e3bc5
zosoist <moh.robati1333@gmail.com>
2020-12-16 14:07:22 +0330

    almost finished

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 let current = 0
+let selected = '#1b262c'
+let unselected = '#3282b8'
+let hover = '#2b373e'
 
 let changeTab = () => {
     Array.from(document.querySelectorAll("#scene-body section")).forEach((el, idx) => {
-        if(idx === current) {
+        if (idx === current) {
             el.style.display = 'block'
         } else {
             el.style.display = 'none'
@@ -11,11 +14,26 @@ let changeTab = () => {
 }
 
 changeTab()
+document.querySelectorAll("#scene-tabbar .list-item")[0].style.backgroundColor = selected
 
 Array.from(document.querySelectorAll("#scene-tabbar .list-item")).forEach((el, idx) => {
     el.addEventListener('click', () => {
-        console.log('hi')
+        Array.from(document.querySelectorAll("#scene-tabbar .list-item")).forEach((e, idx) => {
+            if (e === el) {
+                e.style.backgroundColor = selected
+            } else {
+                e.style.backgroundColor = unselected
+            }
+        })
         current = idx
         changeTab()
+    })
+    el.addEventListener('mouseover', () => {
+        if(current !== idx)
+            el.style.backgroundColor = hover
+    })
+    el.addEventListener('mouseout', () => {
+        if(current !== idx)
+            el.style.backgroundColor = unselected
     })
 })
